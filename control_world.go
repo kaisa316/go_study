b833c63ec11f83864cc27c3a006b6fb538a0a364
kaisa316 <490788241@qq.com>
2019-10-21 15:40:18 +0800

    流程控制

--- control_world.go
+++ control_world.go
@@ -1,3 +1,6 @@
+/**
+* 控制语句学习
+ */
 package main
 
 import (
@@ -19,6 +22,17 @@ func ifStudy() {
 	} else {
 		fmt.Println(a, "grater than 10")
 	}
+}
+
+/**
+* if语句中定义变量
+ */
+func ifSetVal() {
+	//b的作用域仅在if语句中
+	if b := 3; b < 5 {
+		fmt.Println("b小于5")
+	}
+	//fmt.Println(b) //b在这里不可见
 }
 
 /**
@@ -69,18 +83,93 @@ outline:
 }
 
 /**
+* goto 用于跳跃多层循环
+ */
+func forGoto() {
+	//如果是跳转到这里，则成了无限循环
+	//TagTwo:
+	for i := 5; i < 10; i++ {
+		for j := 0; j < 3; j++ {
+			if i == 6 {
+				goto TagOne
+			}
+			fmt.Println(i, ",", j)
+		}
+	}
+TagOne:
+	//循环跳到这里
+}
+
+func forBreakContinue() {
+	/*
+		for i := 0; i < 5; i++ {
+			if i == 3 {
+				break
+			}
+			fmt.Println("I is ", i)
+		}
+
+		fmt.Println("----------------")
+		for j := 0; j < 10; j++ {
+			if j == 5 {
+				continue //跳过当次循环
+			}
+			fmt.Println("j is ", j)
+		}
+	*/
+
+	for i := 0; i < 5; i++ {
+		for j := 1; j < 5; j++ {
+			if i == 2 {
+				break //仅跳出j层循环
+			}
+			fmt.Println(i, ",", j)
+		}
+	}
+}
+
+/**
 * switch 学习
  */
 func switchStudy() {
-	i := 'c'
-	switch i {
-	case 'a', 'c':
-		fmt.Println("china bisheng")
-	case 'b':
-		fmt.Println("american bisheng")
+	exp := 5
+	switch exp {
+	case 4:
+		fmt.Println("equal 4")
+	case 5:
+		fmt.Println("equal 5")
+		fallthrough
+	case 6:
+		fmt.Println("equal 6")
 	default:
-		fmt.Println("default")
-
+		fmt.Println("is default")
 	}
 
+	j := 'c'
+	switch j {
+	case 'a', 'c':
+		fmt.Println("a or c")
+	case 'b':
+		fmt.Println("is b")
+	default:
+		fmt.Println(" is default ")
+	}
+
+}
+
+func test() {
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	j := 10
+	for j < 5 {
+		fmt.Println(j)
+		j--
+	}
+
+}
+
+func main() {
+	forGoto()
 }

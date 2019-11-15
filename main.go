3ec599cf7b6a632996e05cab6fa27d7f41f6a93e
kaisa316 <490788241@qq.com>
2019-11-15 17:57:45 +0800

    go module study

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/kaisa316/nummanip/calc"
-	cc "github.com/kaisa316/nummanip/v2/calc"
 	yy "github.com/kaisa316/nummanip/v3/calc"
 )
 
@@ -11,7 +10,6 @@ func main() {
 	fmt.Println("hello")
 	s := calc.Sum(1, 2, 3)
 	fmt.Println(s)
-	c := cc.Mul(3, 5)
 	fmt.Println(c)
 	yy.Mul(1, 2)
 }

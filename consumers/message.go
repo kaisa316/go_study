65c1f21acc9f08d4544eedeab46841c6c3aa7103
kaisa316 <490788241@qq.com>
2019-11-21 17:15:29 +0800

    十万个任务，同时并发

--- consumers/message.go
+++ consumers/message.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 )
 
+//10万个任务，同时消费N个任务
 func Msg() {
 	total := 100000 //总次数
 	multiNum := 10  //并发数量
 	chanBuffer := make(chan int, multiNum)
-	for i := 0; i < total/multiNum; i++ {
+	for i := 0; i < total; i++ {
 		go work(i, chanBuffer)
 		chanBuffer <- i
 	}
